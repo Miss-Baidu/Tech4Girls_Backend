e6ded969a800422ca57f56dd5309d75f46c7c613
sarahakuabaidu <akuabaidusara@gmail.com>
2024-10-16 06:54:44 +0000

    install_packages.sh

---
+++ Bash_Scripting/install_packages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# this script is to update the package list and install fortune, cowsay, and cmatrix
+sudo apt update
+sudo apt install fortune
+sudo apt install cowsay
+sudo apt install cmatrix
