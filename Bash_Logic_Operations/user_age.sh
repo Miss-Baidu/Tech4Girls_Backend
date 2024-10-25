36e061cec7e37ca395d7e508749b1c901f07dc76
sarahakuabaidu <akuabaidusara@gmail.com>
2024-10-25 06:07:59 +0000

    user_age.sh

---
+++ Bash_Logic_Operations/user_age.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -p "Please enter your age: " age
+if [ "$age" -lt 18 ]; then
+    echo "You are a minor."
+elif [ "$age" -ge 18 ] && [ "$age" -le 64 ]; then
+    echo "You are an adult."
+else
+    echo "You are a senior."
+fi
