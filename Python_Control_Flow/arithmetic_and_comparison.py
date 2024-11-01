efcaaf837e2c46adb1058641390b7cc7cf97f077
sarahakuabaidu <akuabaidusara@gmail.com>
2024-11-01 14:34:32 +0000

    arithmetic_and_comparison.py

---
+++ Python_Control_Flow/arithmetic_and_comparison.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+x = 100
+y = 5
+
+
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+floor_division = x // y
+exponentiation = x ** y
+modulus = x % y
+
+
+equal = x == y
+not_equal = x != y
+less_than = x < y
+greater_than = x > y
+less_than_equal = x <= y
+greater_than_equal = x >= y
+
+
+print(f" {addition}")
+print(f" {subtraction}")
+print(f" {multiplication}")
+print(f" {division}")
+print(f" {floor_division}")
+print(f" {exponentiation}")
+print(f" {modulus}")
+
+
+print(f" {equal}")
+print(f"{not_equal}")
+print(f" {less_than}")
+print(f" {greater_than}")
+print(f"{less_than_equal}")
+print(f"{greater_than_equal}")
